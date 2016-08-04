3aef823c4b42fbf8a1a5c70f72891521d9f2761a
Waleed Hammam <waleed.hammam@gmail.com>
2016-08-04 13:04:58 +0200

    full functionality needs frontend and integration between angular and flask

--- details.py
+++ details.py
@@ -4,15 +4,18 @@ from auth import Auth
 
 class Details(Auth):
     """ SOME COMMENTS HERE """
-    def __init__(self, login, password, state_url, cookie_url, api_link):
+    def __init__(self, login, password, state_url, cookie_url, api_link, data):
         """ SOME COMMENTS HERE """
         super(Details, self).__init__(login, password, state_url, cookie_url)
         self.api_link = api_link
+        self.data = data
 
     def get_details(self):
         """ SOME COMMENTS HERE """
         cookie = self.get_cookie()
         api_url = self.api_link
-
-        result = requests.get(api_url, headers={'Cookie': cookie})
+        if self.data:
+            result = requests.post(api_url, headers={'Cookie': cookie}, data={'nid': self.data})
+        else:
+            result = requests.get(api_url, headers={'Cookie': cookie})
         return result.text
--- server.py
+++ server.py
@@ -1,13 +1,13 @@
 """ SOME COMMENTS HERE """
-from flask import Flask
+from flask import Flask, send_from_directory, render_template, Response, request
 from details import Details
 import json, os
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder="client")
 
 
 """ Server API"""
-def helper(api):
+def helper(api, nid):
     """ Extract config data """
     with open('.config.json') as json_data_file:
         data = json.load(json_data_file)
@@ -21,37 +21,51 @@ def helper(api):
     cookie_url = auth['cookie_url']
 
     apis = data['apis']
-    api_link = apis[api]
+    if api == 'getDetailedStatus':
+        api_link = apis[api] + str(nid)
+    else:
+        api_link = apis[api]
 
-    details = Details(login, password, state_url, cookie_url, api_link)
+    if api == "getHealthRun":
+        details = Details(login, password, state_url, cookie_url, api_link, nid)
+    else:
+        details = Details(login, password, state_url, cookie_url, api_link, '')
 
     return details.get_details()
 
 @app.route("/")
 def main_page():
-    """ SOME COMMENTS HERE """
-    return 'main'
+    return 'main_page'
 
-@app.route("/getDetailedStatus")
-def getDetailedStatus(node):
+@app.route("/getDetailedStatus", methods=["GET", "POST"])
+def getDetailedStatus():
     """ SOME COMMENTS HERE """
-    return helper('getDetailedStatus' + node)
+    nid = request.args.get('nid_no')
+    result = Response(helper('getDetailedStatus', nid))
+    result.headers['Access-Control-Allow-Origin'] = '*'
+    return result
 
 @app.route("/getStatusSummary")
 def getStatusSummary():
     """ SOME COMMENTS HERE """
-    return helper('getStatusSummary')
+    result = Response(helper('getStatusSummary', ''))
+    result.headers['Access-Control-Allow-Origin'] = '*'
+    return result
 
 @app.route("/getOverallStatus")
 def getOverallStatus():
     """ SOME COMMENTS HERE """
-    return helper('getOverallStatus')
+    result = Response(helper('getOverallStatus', ''))
+    result.headers['Access-Control-Allow-Origin'] = '*'
+    return result
 
-@app.route("/getHealthRun")
+@app.route("/getHealthRun", methods=["GET", "POST"])
 def getHealthRun():
     """ SOME COMMENTS HERE """
-    return helper('getHealthRun')
-
+    nid = request.args.get('nid_no')
+    result = Response(helper('getHealthRun', nid))
+    result.headers['Access-Control-Allow-Origin'] = '*'
+    return result
 
 """ SOME COMMENTS HERE """
 if __name__ == "__main__":
