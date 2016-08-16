8dde116d25d362caeb8c1909bcad38819f044233
Mohammed Ashour <m.aly.ashour@gmail.com>
2016-08-16 12:01:48 +0200

    deleting some unnecessary code

--- ClientApp/app/dash/dash.component.ts
+++ ClientApp/app/dash/dash.component.ts
@@ -30,16 +30,6 @@ export class DashComponent implements OnInit {
         this.getAllData(envs,this.timeoutTheDetails.bind(this,envs))
    });
 
-    // this.id = setInterval(() => {
-
-    //   // for (let env in this.Environments){
-    //   //   this.getOverallStatus(env);
-    //   //   if(this.Environments[env]['expanded'] == true){
-    //   //     this.getStatusSummary(env);
-    //   //   }
-    //   // }
-    // }, 1000);
-
 
   }
 
@@ -47,17 +37,7 @@ export class DashComponent implements OnInit {
     console.log("envs=> "+ envs)
     console.log("callback => " +callback)
     Promise.all(envs.map(this.getOverallStatus.bind(this))).then(callback)
-   
-    //     console.log("going in allData")
-    //     let all_promises ;
-    //     for (let env in this.Environments) {
-    //       all_promises[env] =  this.getOverallStatus(env).then(() => {
-    //         if (this.Environments[env]['expanded']) {
-    //           return this.getStatusSummary(env);
-    //         }
-    // });
-    // return all_promises.toPromise();
-    //     }
+
   
 }
 
@@ -68,24 +48,17 @@ export class DashComponent implements OnInit {
 
   // gets the state of every machine        
   getOverallStatus(environment) {
-    console.log("environment ====>" + environment)
-    // console.log(".map(x => x.namehere!")
-    // console.log(environment)
+
     return this.dashService.getOverallStatus(environment).then(
       (response) => {
-        // console.log("this", this)
         let expanded = this.Environments[environment].expanded
         console.log("Enironments Expanded " + expanded)
         this.OverallStatus[environment] = response.json()
         this.Environments[environment]['state'] = this.OverallStatus[environment]['state']
         this.Environments[environment].expanded = expanded
-        // console.log(this.Environments[environment])
-        // console.log("--------")
-        // console.log(expanded)
         function is_expanded(env) {
           return this.Environments[env].expanded
         }
-        console.log("keys=>>>>" + Object.keys(this.Environments))
         return Object.keys(this.Environments)
       }
     ).then((environments) => {
@@ -99,9 +72,7 @@ export class DashComponent implements OnInit {
     return this.dashService.getStatusSummary(environment).then((response) => {
       let env = this.Environments[environment]
       //check and get DetailedStatus
-      console.log("env " + env.summary)
       let expanded = env.summary ? env.summary.map(m => m.expanded) : [];
-      console.log("expanded " + expanded)
       let details = env.summary ? env.summary.map(m => m.details) : [];
       let summary = response.json();
 
@@ -111,9 +82,7 @@ export class DashComponent implements OnInit {
       }
 
       for (let machine in summary) {
-        console.log(expanded[machine])
         if (expanded[machine]) {
-          console.log("the summary oif machines" + summary[machine])
         }
       }
 
@@ -122,7 +91,6 @@ export class DashComponent implements OnInit {
       }
 
       let expanded_machines = summary.filter(is_expanded)
-      console.log(expanded_machines)
       env.summary = summary
       return expanded_machines
     }).then((expanded_machines) => { return Promise.all(expanded_machines.map(this.getDetailedStatus.bind(this, environment))) });
@@ -130,7 +98,6 @@ export class DashComponent implements OnInit {
 
   // gets a detailed info of a machine
   getDetailedStatus(environment, machine) {
-    console.log("I'm IN DETAILED")
     return this.dashService.getDetailedStatus(environment, machine.nid).then((response) => {
       let expanded = machine.details ? machine.details.map(m => m.expanded) : [];
       machine.details = response.json();
@@ -155,39 +122,3 @@ export class DashComponent implements OnInit {
 
 }
 
-
-// getEverything(){
-//   return dash.getEverything().then( (value) => {
-//     saveEverything(value)
-//     return environments
-//   }).map(this.getEnvironment)
-// }
-// getEnvironment(environment){
-//   return dash.getEnvironment(environment).then( (value) => {
-//     saveEnvironment(environment, value)
-//     return environment.machines
-//   }).map(this.getMachine)
-// }
-// getMachine(machine){
-//   return dash.getMachine(machine).then( (value) => {
-//     saveMachine(machine, value)
-//     return machine.services
-//   }).map(this.getService)
-// }
-// getService(service){
-//   return dash.getService(service).then( (value) => {
-//     saveService(service, value)
-//     return service.component
-//   }).map(this.getComponent)
-// }
-
-// .then(() => {
-//   return environments
-// }).then((Environments) => {
-//   return Promise.all(environments.map(this.getEnvironment))
-// })
-
-// function x(){
-//   return setTimeout(x.bind(null, callback), 1000)
-// }
-// getEverything.then(setTimeout)
